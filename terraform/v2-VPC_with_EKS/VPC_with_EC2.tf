8383d5331a5b0a5f39a9dc8c04f72849ccd127a3
avinashbasoor12 <34620046+avinashbasoor12@users.noreply.github.com>
2024-02-04 02:36:49 +0530

    Update VPC_with_EC2.tf
    update the diffrent zones

--- terraform/v2-VPC_with_EKS/VPC_with_EC2.tf
+++ terraform/v2-VPC_with_EKS/VPC_with_EC2.tf
@@ -112,7 +112,7 @@ module "eks" {
   source     = "./eks"
   vpc_id     = aws_vpc.demo-vpc.id
   sg_ids     = module.sgs.security_group_public
-  subnet_ids = [aws_subnet.demo_subnet2.id, aws_subnet.demo_subnet2.id]
+  subnet_ids = [aws_subnet.demo_subnet1.id, aws_subnet.demo_subnet2.id]
 
 }
 
